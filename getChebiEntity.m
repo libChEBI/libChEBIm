42645dfb317a6a7481fa2d5db66704b5d9b73d7d
Neil Swainston <neil.swainston@manchester.ac.uk>
2015-10-05 16:24:24 +0100

    Repackaged libChEBIm, and check for multiple assignment of Java path.

--- getChebiEntity.m
+++ getChebiEntity.m
@@ -1,4 +1,20 @@
 function chebiEntity = getChebiEntity(id)
-    javaaddpath('libChEBI.jar');
-    chebiEntity = javaObject('libchebi.ChebiEntity', id);
+    dpath = javaclasspath('-dynamic');
+    paths = size(dpath, 1);
+    found_libchebi = 0;
+    
+    for i = 1:paths;
+        path = dpath{i, end};
+        [~, name, ext] = fileparts(path);
+        
+        if strcmp(name, 'libChEBIj-1.0.0') && strcmp(ext, '.jar')
+            found_libchebi = 1;
+        end
+    end
+
+    if ~found_libchebi
+        javaaddpath('libChEBIj-1.0.0.jar');
+    end
+    
+    chebiEntity = javaObject('uk.ac.manchester.libchebi.ChebiEntity', id);
 end
